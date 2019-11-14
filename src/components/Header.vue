0754bd3e5a40504a828acfa693a7939c84a7eaa4
Jessa <jessa@mallowfields.com>
2019-11-14 09:53:43 -0500

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled><b>Random Science Questions Quiz App</b></b-nav-item>
+            <b-nav-item disabled>Directions: See how many you can get right out of 10 questions.</b-nav-item>
+            <b-nav-item disabled><b>Counter: {{ numCorrect }}/{{ numTotal }}</b></b-nav-item>
+            
+        </b-nav>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
